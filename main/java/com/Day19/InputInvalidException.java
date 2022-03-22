d0456426ca74dc8870f6ee3db07afbe5e8d50b5d
KAVATKAR <kavatkar-vishal.prabhakar@capgemini.com>
2022-03-22 21:54:30 +0530

    Lambda Function To Validate User

--- main/java/com/Day19/InputInvalidException.java
+++ main/java/com/Day19/InputInvalidException.java
@@ -5,7 +5,7 @@ package com.Day19;
  *  We have used Inheritance and extended the Exception class.
  *  Then we have created a method and passed a message as input.
  *  Inside the method we have called the super. It is a reference variable which is used to refer immediate parent class object. 
- * @author Tom
+ * 
  *
  */
 public class InputInvalidException extends Exception {
